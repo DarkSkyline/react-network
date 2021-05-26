e9585d56ab3816752194adf8adc7fb9ef0332103
Filipe Ferreira <filipesferreira94@gmail.com>
2021-05-26 20:59:44 +0100

    Feat: add create post

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Feed from './components/Feed'
 import Header from './components/Header'
+import CreatePost from './components/posts/CreatePost'
 
 function App() {
 
   return (
     <>
       <Header />
+      <CreatePost />
       <Feed />
      </>
   )
---
+++ src/components/posts/CreatePost.js
@@ -0,0 +1,9 @@
+function CreatePost() {
+    return (
+        <>
+            <input type="text" placeholder="Title of Post"/>
+        </>
+    )
+}
+
+export default CreatePost
